b0c7d4d5da8d369fe2a898a944bc464f9d61eac2
Mustafa Hussien <mustafa.hussien100@gmail.com>
2022-07-08 14:45:31 +0200

    Adding stopwatch functions

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,42 @@
-const buttonStart = document.querySelector('.btn-start');
-const buttonStop = document.querySelector('.btn-stop');
-const buttonReset = document.querySelector('.btn-reset');
-let timeSpan = document.querySelector('.time');
+const buttonStart = document.querySelector('.btn-start'),
+    buttonStop = document.querySelector('.btn-stop'),
+    buttonReset = document.querySelector('.btn-reset');
+let timeSpan = document.querySelector('.time'),
+    stopTime = true;
+
+let sec = 0,
+    min = 0,
+    hr = 0;
+
+buttonStart.addEventListener('click', stopTime = false)
+buttonStop.addEventListener('click', stopTime = true)
+
+function timerCycle() {
+    if (stopTime = false) {
+        sec = parseInt(sec);
+        min = parseInt(min);
+        hr = parseInt(hr);
+
+        sec += 1;
+        if (sec < 10) {
+            sec = '0' + sec;
+        } else if (sec === 60) {
+            sec = 0;
+            min += 1;
+        }
+
+        if (min < 10) {
+            min = '0' + min;
+        } else if (min === 60) {
+            min = 0;
+            hr += 1;
+        }
+
+        if (hr < 10) {
+            hr = '0' + hr;
+        }
+
+        setTimeout(timerCycle, 1000);
+        timeSpan.innerHTML = `${hr}:${min}:${sec}`
+    }
+}
