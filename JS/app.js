97e9cc580b090990a71e7889072e9bd22ec1affe
Mustafa Hussien <mustafa.hussien100@gmail.com>
2022-07-08 12:43:28 +0200

    Adding DOM element time variable

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,4 @@
-const buttonStart = document.querySelector('.btn-start')
-const buttonStop = document.querySelector('.btn-stop')
-const buttonReset = document.querySelector('.btn-reset')
+const buttonStart = document.querySelector('.btn-start');
+const buttonStop = document.querySelector('.btn-stop');
+const buttonReset = document.querySelector('.btn-reset');
+let timeSpan = document.querySelector('.time');
